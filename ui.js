07f8329f4099c95d9fe5c86e3421077143088c06
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-05-01 22:15:18 +0200

    amazing

--- ui.js
+++ ui.js
@@ -30,7 +30,7 @@ function nextPhase() {
   } else {
     currentWave++;
     if (currentWave > maxWaves) {
-      gameOver();
+      loopDone();
       return;
     }
     currentPhase = 'build';
@@ -41,25 +41,25 @@ function nextPhase() {
 
 
 function updateUI() {
-  const bottomMiddle = document.querySelector('.bottom-middle');
-  const topMiddle = document.querySelector('.top-middle > .timer');
+  //const bottomMiddle = document.querySelector('.bottom-middle');
+  const topMiddle = document.querySelector('.timer');
   const image = document.querySelector(`#wave-${currentWave}`);
   const line1 = document.querySelector('.line-1');
   const line2 = document.querySelector('.line-2');
   const line3 = document.querySelector('.line-3');
 
-  const line1WrapperA = document.querySelector('.types-line-1 .image-wrapper-attack');
-  const line1WrapperD = document.querySelector('.types-line-1 .image-wrapper-defense');
-  const line2WrapperA = document.querySelector('.types-line-2 .image-wrapper-attack');
-  const line2WrapperD = document.querySelector('.types-line-2 .image-wrapper-defense');
-  const line3WrapperA = document.querySelector('.types-line-3 .image-wrapper-attack');
-  const line3WrapperD = document.querySelector('.types-line-3 .image-wrapper-defense');
+  //const line1WrapperA = document.querySelector('.types-line-1 .image-wrapper-attack');
+  //const line1WrapperD = document.querySelector('.types-line-1 .image-wrapper-defense');
+  //const line2WrapperA = document.querySelector('.types-line-2 .image-wrapper-attack');
+  //const line2WrapperD = document.querySelector('.types-line-2 .image-wrapper-defense');
+  //const line3WrapperA = document.querySelector('.types-line-3 .image-wrapper-attack');
+  //const line3WrapperD = document.querySelector('.types-line-3 .image-wrapper-defense');
 
   if (currentPhase === 'build') {
     line1.textContent = `Prepare for`;
     line2.textContent = `WAVE ${currentWave}`;
     line3.textContent = ``;
-
+/*
     let attack = wavesData[Math.min(currentWave, 21)].dmgType;
     let defense = wavesData[Math.min(currentWave, 21)].defType;
 
@@ -67,7 +67,7 @@ function updateUI() {
     line2WrapperD.innerHTML = ` <img style="margin-left: 0.6vw;" class="img" src='img/types/${defense}.png' alt='Defense ${defense}' /> `
     line3WrapperA.innerHTML = ``
     line3WrapperD.innerHTML = ``
-
+*/
     topMiddle.textContent = `${remainingTime}`;
     image.style.border = '4px solid black';
 
@@ -81,13 +81,13 @@ function updateUI() {
     line1.textContent = ``;
     line2.textContent = `WAVE ${currentWave}`;
     line3.textContent = `Next: WAVE ${currentWave + 1}`;
-
+/*
     let attack = wavesData[Math.min(currentWave + 1, 21)].dmgType;
     let defense = wavesData[Math.min(currentWave + 1, 21)].defType;
 
     line3WrapperA.innerHTML = ` <img style="width: 1vw; height: 1vw; margin-right: 0.3vw;" src='img/types/${attack}.png' alt='Damage ${attack}' /> `
     line3WrapperD.innerHTML = ` <img style="width: 1vw; height: 1vw; margin-left: 0.3vw;" src='img/types/${defense}.png' alt='Defense ${defense}' /> `
-
+*/
     topMiddle.textContent = '';
     image.style.border = '4px solid red';
   }
@@ -95,7 +95,7 @@ function updateUI() {
 
 function moveImage(wave) {
   const image = document.querySelector(`#wave-${wave}`);
-  const middle = document.querySelector('.top-middle');
+  const middle = document.querySelector('.top-row.mid-col');
   
   middle.appendChild(image);
   image.style.position = 'absolute';
@@ -109,42 +109,16 @@ function moveImage(wave) {
 
 function moveToTopLeft(wave) {
   const image = document.querySelector(`#wave-${wave}`);
-  const target = document.querySelector('.top-left');
+  const target = document.querySelector('.top-row.left-col');
   image.style.transition = 'none';
   image.style.border = '';
   image.style.position = 'static';
   target.appendChild(image);
-  applyFadingEffect();
 }
 
 
-function gameOver() {
+function loopDone() {
   clearInterval(phaseTimer);
-  const bottomMiddle = document.querySelector('.bottom-middle');
-  bottomMiddle.textContent = 'Game Over';
-}
-
-function applyFadingEffect(fadeThreshold = 3) {
-  const topRight = document.querySelectorAll('.top-right > img');
-  const topLeft = document.querySelectorAll('.top-left > img');
-
-  for (let i = 0; i < topRight.length; i++) {
-    const distanceFromCurrent = Math.abs(currentWave - (i + 1));
-    if (distanceFromCurrent >= fadeThreshold) {
-      topRight[i].style.opacity = 0;
-    } else {
-      topRight[i].style.opacity = 1 - (distanceFromCurrent / fadeThreshold);
-    }
-  }
-
-  for (let i = topLeft.length - 1, j = 0; i >= 0; i--, j++) {
-    const distanceFromCurrent = Math.abs(currentWave - (j + 1));
-    if (distanceFromCurrent >= fadeThreshold) {
-      topLeft[i].style.opacity = 0;
-    } else {
-      topLeft[i].style.opacity = 1 - (distanceFromCurrent / fadeThreshold);
-    }
-  }
 }
 
 
@@ -153,12 +127,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const uiContainer = document.querySelector('.ui-container');
   const uiImage = document.querySelector('.ui-image');
   const wavesIcons = document.querySelector('.ui-container .ui-text .grid-item.top-row.right-col');
-  const pastWavesIcons = document.querySelector('.ui-container .ui-text .grid-item.top-row.left-col');
+  //const pastWavesIcons = document.querySelector('.ui-container .ui-text .grid-item.top-row.left-col');
 
   const timerText = document.querySelector('.timer');
 
   const updateDimensions = () => {
-    const containerHeight = uiContainer.clientHeight;
+    //const containerHeight = uiContainer.clientHeight;
     const containerWidth = uiImage.clientWidth;
     uiContainer.style.width = `${containerWidth}px`;
     const fontSize = containerWidth * 0.025; // Adjust the multiplier to change the font size
@@ -175,15 +149,16 @@ document.addEventListener('DOMContentLoaded', () => {
     img.classList.add('wave-icon');
     wavesIcons.appendChild(img);
 
+    /*
     const img2 = document.createElement('img');
     img2.src = `img/${i}.png`;
     img2.id = `wave--${i}`;
     img2.classList.add('wave-icon');
-    pastWavesIcons.appendChild(img2);
+    pastWavesIcons.appendChild(img2);*/
   }
 
   updateDimensions();
   window.addEventListener('resize', updateDimensions);
 
-  //startUILoop();
+  startUILoop();
 });
