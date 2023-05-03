0c236ea352756d8da8cbcd8bb7a0c61a70c14f5b
Alexandre 'Kidev' Poumaroux <1204936+Kidev@users.noreply.github.com>
2023-05-03 18:14:15 +0200

    Getting there

--- js/waves.js
+++ js/waves.js
@@ -39,11 +39,7 @@ function createImageContainer(index) {
     const container = document.createElement('div');
 
     container.id = `wave-$(index)`;
-    container.className = 'wave-icons-container';
-    container.zIndex = '1';
-
-    container.style.position = 'relative';
-    container.style.display = 'inline-block';
+    container.className = 'wave-container';
 
     container.addEventListener('mousedown', event => {
         if (event.buttons & 1) {
@@ -60,41 +56,23 @@ function createImageContainer(index) {
     // Main image
     const mainImage = document.createElement('img');
     mainImage.src = `/img/${index}.png`;
-    mainImage.className = 'wave-icon';
+    mainImage.className = 'wave-main-image';
 
     container.appendChild(mainImage);
 
     // Corner images
-    const corners = ['top-left', 'top-right', 'bottom-left', 'bottom-right'];
-    const displayCornerImages = [false, false, false, false]; // Change this to control the display of corner images
+    const cornersClasses = ['wave-corner-0', 'wave-corner-1', 'wave-corner-2', 'wave-corner-3'];
+    cornersClasses.forEach((corner, corner_id) => {
+        const detailImage = document.createElement('img');
+        detailImage.src = '/img/icons/1_yellow.png';
+        detailImage.classList.add('wave-corner-image', corner);
 
-    corners.forEach((corner, i) => {
-        if (displayCornerImages[i]) {
-            const detailImage = document.createElement('img');
-            detailImage.src = '/img/icons/1_yellow.png';
+        // Tint the image
+        detailImage.style.filter = `hue-rotate(${playerColorsHueRotateFromYellow[corner_id]});`;
+        detailImage.style.display = 'none';
 
-            // Tint the image
-            detailImage.style.filter = `hue-rotate(${playerColorsHueRotateFromYellow[i]});`;
-
-            // Set corner positions
-            if (corner.includes('top')) {
-                detailImage.style.top = '0';
-            } else {
-                detailImage.style.bottom = '0';
-            }
-
-            if (corner.includes('left')) {
-                detailImage.style.left = '0';
-            } else {
-                detailImage.style.right = '0';
-            }
-
-            detailImage.className = `wave-icon-${corners[i]}`;
-
-            container.appendChild(detailImage);
-        }
+        container.appendChild(detailImage);
     });
-
     return container;
 }
 
